82a531069a2e07ee7af8167bc56894129500345a
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2019-10-15 18:40:12 -0700

    converter of Pounds to kilograms

---
+++ PoundsToKilograms/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //int pound = 3;
+        float pound = 3f;
+        double kilogram = pound * 0.45359237;
+        System.out.println(kilogram + "kg");
+    }
+}
